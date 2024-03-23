e46669b4a8be1094f1eef0272981b6ca2492e2fc
Kelly Chang <kchang4@uw.edu>
2024-03-22 18:37:13 -0700

    edit tfrecord to take one csv

--- tfrecord.py
+++ tfrecord.py
@@ -21,9 +21,9 @@ def _tfrecord_example(image, label):
   }))
 
 
-def _process_split_csv(dataset_csv):
+def _process_csv(csv_fname):
   """Processes image and label pairs from input csv file."""
-  return [(x.image, x.label) for x in pd.read_csv(dataset_csv).itertuples()]
+  return [(x.image, x.label) for x in pd.read_csv(csv_fname).itertuples()]
 
 
 def _write_tfrecord(example_list, shard_file_pattern,
@@ -49,39 +49,32 @@ def _write_tfrecord(example_list, shard_file_pattern,
     print(f"  Saved {n+1:03d} out of {n_shards:03d} shards...")
 
 
-def create_tfrecord_dataset(
-  train_csv, test_csv, valid_csv, 
-  output_dir = os.getcwd(),        
-  examples_per_shard = 128, 
-  compression_type = "GZIP"):
+def create_tfrecord_dataset(file_prefix, csv_fname, output_dir = os.getcwd(),        
+                            examples_per_shard = 128, compression_type = "GZIP"):
   """Creates a TFRecord dataset from input CSV files."""
 
   # create local output directory (if does not exists)
   os.makedirs(output_dir, exist_ok = True)
 
-  # create split dictionary from csv files
-  split_dict = {"train": train_csv, "test": test_csv, "valid": valid_csv}
-  for split_name, csv in split_dict.items():
-    split_dict[split_name] = _process_split_csv(csv)
+  # create example list from csv file
+  example_list = _process_csv(csv_fname)
       
-  # write split tfrecords by shards
-  for split_name, example_list in split_dict.items():
-    # write dataset tfrecord files
-    print(f"Processing '{split_name}' split...")
-    shard_name = f"{split_name}" + "_shard-{shard:03d}.tfrec"    
-    _write_tfrecord(
-      example_list       = example_list, 
-      shard_file_pattern = op.join(output_dir, shard_name), 
-      examples_per_shard = examples_per_shard, 
-      compression_type   = compression_type
-    )
+  # write dataset tfrecord files
+  print(f"Processing '{file_prefix}' dataset...")
+  shard_name = f"{file_prefix}" + "_shard-{shard:03d}.tfrec"    
+  _write_tfrecord(
+    example_list       = example_list, 
+    shard_file_pattern = op.join(output_dir, shard_name), 
+    examples_per_shard = examples_per_shard, 
+    compression_type   = compression_type
+  )
 
       
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
-  parser.add_argument("--train_csv", type = str, required = True)
-  parser.add_argument("--test_csv", type = str, required = True)
-  parser.add_argument("--valid_csv", type = str, required = True)
+  parser.add_argument("--file_prefix", type = str, required = True, 
+                      choices = ["train", "test", "valid"])
+  parser.add_argument("--csv_fname", type = str, required = True)
   parser.add_argument("--output_dir", type = str, default = os.getcwd())
   parser.add_argument("--examples_per_shard", type = int, default = 128)
   parser.add_argument("--compression_type", type = str, default = "GZIP")
@@ -89,18 +82,16 @@ if __name__ == "__main__":
           
   # print argument information
   print("\nStarting TFRecord Creation...")
-  print(f"  -> Training CSV: {args.train_csv}")
-  print(f"  -> Validation CSV: {args.valid_csv}")
-  print(f"  -> Test CSV: {args.test_csv}")
+  print(f"  -> File Prefix: {args.file_prefix}")
+  print(f"  -> CSV Filename: {args.csv_fname}")
   print(f"  -> Output Directory: {args.output_dir}")
   print(f"  -> Examples per Shard: {args.examples_per_shard}")
   print(f"  -> Compression Type: {args.compression_type}\n")
   
   # create tfrecord dataset (with sharding)
   create_tfrecord_dataset(
-    train_csv          = args.train_csv, 
-    test_csv           = args.test_csv, 
-    valid_csv          = args.valid_csv,
+    file_prefix        = args.file_prefix,
+    csv_fname          = args.csv_fname, 
     output_dir         = args.output_dir,
     examples_per_shard = args.examples_per_shard, 
     compression_type   = args.compression_type
